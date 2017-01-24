aaba284c149e8cb929be31d4e843d1bc6efb4c5f
IceFractal <jdenea@outlook.com>
2017-01-23 16:58:31 -0800

    add tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,25 @@
 const assert = require('assert');
+const ttt = require('../logic/ttt');
+
+const board = (new Array(9)).fill(0);
 
 describe('Tic Tac Toe', () => {
-  it('should know that 2 + 2 != 5', () => {
-    assert.notEqual(2 + 2, 5);
+  beforeEach(() => {
+    ttt.init();
+  });
+
+  it('should generate a blank board', () => {
+    assert.deepEqual(ttt.init(), board);
+  });
+
+  it('should take input', () => {
+    const result = board.slice();
+    result[2] = 1;
+    assert.deepEqual(ttt.input(2), result);
+  });
+
+  it('should reset the board on init', () => {
+    ttt.input(2);
+    assert.deepEqual(ttt.init(), board);
   });
 });
